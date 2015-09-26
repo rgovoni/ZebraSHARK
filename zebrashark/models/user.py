1195436b3053077b017a798f30323bd30706b39e
Gavin Bisesi <gavin@curata.com>
2015-09-26 11:47:13 -0400

    username should not be unique

--- zebrashark/models/user.py
+++ zebrashark/models/user.py
@@ -5,7 +5,7 @@ from zebrashark.models.question import Stance
 
 class User(Model):
     email_address = String(required=True, unique=True)
-    name = String(required=True, unique=True)
+    name = String(required=True)
     hash = String(required=True)
     conversation_entries = OneToMany('ConversationEntry')
     conversations = OneToMany('ConversationParticipant')
