a0aedff2868688bff24d70bdd24784106882acd6
rgovoni <govoni.r@husky.neu.edu>
2015-09-26 01:50:18 -0400

    Set up main pages

--- zebrashark/app.py
+++ zebrashark/app.py
@@ -4,16 +4,29 @@ import flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "sugar"
+def index():
+    return render_template('index.html')
+
 @app.route("/test/<value>")
 def echo(value):
     response = {"input": value}
     return flask.jsonify(response)
 
-@app.route("/index")
-def index():
-    return render_template('index.html')
+@app.route("/about")
+def about():
+    return "this is what is up"
+
+@app.route("/conversation")
+def conversation():
+    return render_template('conversation.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
+@app.route("/profile")
+def profile():
+    return render_template('profile.html')
 
 if __name__ == "__main__":
     app.run()
